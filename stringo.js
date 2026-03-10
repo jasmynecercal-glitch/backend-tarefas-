1b7683da360aea412493b7fb6f9d1f2a542fa35d
jasmynecercal-glitch <jasmyne.cercal@escola.pr.gov.br>
2026-03-10 08:50:05 -0300

    Create stringo.js

---
+++ stringo.js
@@ -0,0 +1,12 @@
+console.log ("ENDEREÇO COMPLETO");
+
+let rua = " Rua fas Flores";
+let numero = 9851;
+let cidade = " Curitiba";
+let cep = " 80010-00" ;
+
+let enderecoCompleto = rua + "." = numero + "-" + cidade + "-" = cep;
+
+console.log ( EndereçoCompleto)
+
+//Adicione informaçoes
