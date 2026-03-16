87024d2c0dba60858bb3224aac5a2caded774f61
jasmynecercal-glitch <jasmyne.cercal@escola.pr.gov.br>
2026-03-16 13:52:03 +0000

    versao1

--- hora.js
+++ hora.js
@@ -1,1 +1,17 @@
-//Valores iniciais do dia
+// Valores iniciais do dia
+let valorPorHora = 50;
+let horasTrabalhadas = 8; // Expediente padrão
+
+console.log("--- Resumo do Expediente ---");
+console.log(`Horas iniciais: ${horasTrabalhadas}h`);
+
+horasTrabalhadas++;
+console.log(`Após hora extra (horasTrabalhadas++): ${horasTrabalhadas}h`);
+
+horasTrabalhadas--;
+console.log(`Após saída antecipada: ${horasTrabalhadas}h`);
+
+
+//calculo final
+let taxaSistema = 15;
+let pagamentoTotal = (horasTrabalhadas * valorPorHora) - taxaSistema;
