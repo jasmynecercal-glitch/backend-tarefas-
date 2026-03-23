d36de2dfeb102ff110f4b51448205e5da5b811ae
jasmynecercal-glitch <jasmyne.cercal@escola.pr.gov.br>
2026-03-23 13:23:41 +0000

    vtrdse

---
+++ temperatura.js
@@ -0,0 +1,13 @@
+//Conversão de temperturas
+
+const cidade ="Curitiba";
+const tempCelsius = 18.5;
+
+const tempFahrenheit =temCelsiu * (9/5)+ 32;
+const tempKelvin = temCelsiu + 273.15;
+const variacaoDiaria = 26.3 - temCelsiu;
+
+console.log(`Temperatura em ${cidade} hoje está ${tempCelsius}.`);
+console.log(`Celsius: ${tempCelsius}`);
+
+//Fazer console.log para Fahrenheit e para Kelvin
