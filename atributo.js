12d8a76cebd264d007d002ca120ac363ef6b8961
jasmynecercal-glitch <jasmyne.cercal@escola.pr.gov.br>
2026-03-24 13:53:07 +0000

    versao1

---
+++ atributo.js
@@ -0,0 +1,18 @@
+let numero = 2;
+numero = numero * 2;
+
+console.log(numero)
+
+let numeroN = 2;
+numeroN *= 2;
+console.log(numeroN);
+
+//comparando numeros
+const idadeDandara = 26;
+const minhaIdade = 16;
+
+const saoIguais = idadeDandara == minhaIdade;
+console.log(`As idades sao iguais?`+ saoIguais);
+
+const comparacao = idadeDandara > minhaIdade;
+console.log()
