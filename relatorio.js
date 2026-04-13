d389fbf1588b3eabc16964679c4568743ca7a334
jasmynecercal-glitch <jasmyne.cercal@escola.pr.gov.br>
2026-04-13 11:34:42 +0000

    versao3

---
+++ relatorio.js
@@ -0,0 +1,13 @@
+//Relatório de notas
+console.log("Relatório de Notas");
+
+let aluno = "Alice";
+let nota1 = 9;
+let nota2 = 2;
+let nota3 = 6;
+let media = (nota1 + nota2 + nota3) / 3;
+
+//let relatorio = "Aluno: " + aluno + "\n" + "Notas: " + nota1 + ", " + nota2 + ", " + nota3;
+let relatorio = "Aluno: " + aluno + "| Notas: " + nota1 + "e" + nota2 + "| Media: " + media;
+
+console.log(relatorio);
